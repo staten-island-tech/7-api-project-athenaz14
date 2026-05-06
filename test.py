83e80fce50f5cd413e6526974f2fc355ca9d179f
athenaz14 <athenaz11@nycstudents.net>
2026-05-05 21:12:50 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,17 @@
-import holidayapi
-key = '3fea9852-6653-49c3-94f0-ed12cddbac0f'
-hapi = holidayapi.v1(key)
-holidays = hapi.holidays({
-  'country': 'US',
-  'year': '2025',
-})
+import requests
+def getholiday(holi):
+    response = requests.get(f"https://holidayapi.com/v1/holidays?pretty&key=3fea9852-6653-49c3-94f0-ed12cddbac0f&country=US&year=2025")
+    if response.status_code != 200:
+        print("Error fetching data!")
+        return None
+    data = response.json()
+    
+    print(data["name"])
+
+    return []
+""" 
+            "name": data["name"],
+            "date": data["date"],
+            "observed": data["observed"],
+            "public": data["public"],
+            "country": data["country"] """
