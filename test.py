f7a380a87ef09c0b4e53ffca42440566d4a8c30e
athenaz14 <athenaz11@nycstudents.net>
2026-05-12 21:32:53 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,24 +1,26 @@
 import requests
 url = "https://api.chess.com/pub/player/124chess"
-def getMatch(chess):
+def getMatch():
     response = requests.get(url)
     if response.status_code != 200:
         print("Error fetching data!")
         return None
-    
-    data = response.json()
-    return {
-
-        "avatar" : data["avatar"],
-        "player_id" : data["player_id"],
-        "@id" : data["@id"],
-        "url" : data["https://www.chess.com/member/124chess"],
-        "name" : data["name"],
-        "title" : data["title"],
-
-    }
-user_input = input("Learn more about chess player: 124chess(options are avatar, player_id, @id, url, name, title)")
-if user_input == "avatar":
-    print(data["avatar"])
+    return []
+data = getMatch()    
+""" user_input = input("Learn more about chess player: 124chess(options are avatar, player_id, @id, url, name, title)")
+ """
+for info in data:
+    print(info)
 
 
+""" 
+for info in data:
+    if user_input == data["avatar"]:
+        print(data["avatar"])
+    elif user_input == data["player_id"]:
+         print(data["player_id"])
+    elif user_input == data["@id"]:
+         print(data["@id"])
+
+
+ """
